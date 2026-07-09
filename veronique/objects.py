dd623819f0ad6d03d7ecedbccf68d8c3b1653a32
L3viathan <git@l3vi.de>
2026-07-09 22:15:03 +0200

    Bugfix: wrong table name

--- veronique/objects.py
+++ veronique/objects.py
@@ -661,7 +661,7 @@ class Claim(Model):
         cur = db.conn.cursor()
         cur.execute("DELETE FROM claims WHERE id = ?", (self.id,))
         cur.execute("DELETE FROM forward_index WHERE table_name = 'claims' AND id = ?", (self.id,))
-        cur.execute("DELETE FROM reverse_index WHERE table_name = 'claims' AND id = ?", (self.id,))
+        cur.execute("DELETE FROM inverted_index WHERE table_name = 'claims' AND id = ?", (self.id,))
         # evict deleted claim from cache:
         db.conn.commit()
         self._cache.pop(self.id)
